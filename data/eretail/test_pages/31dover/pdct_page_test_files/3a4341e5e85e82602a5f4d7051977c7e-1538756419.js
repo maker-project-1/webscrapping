eb31e832a19f093912f0553ea4a39d931f448a75
Pierre <pierre.chevalier@epitech.eu>
2019-05-05 19:48:01 +0200

    without useless files

--- data/eretail/test_pages/31dover/pdct_page_test_files/3a4341e5e85e82602a5f4d7051977c7e-1538756419.js
+++
@@ -1,2 +0,0 @@
-
-window.FreeGift={Models:{},Collections:{},Views:{},$:null};head.js(mw_baseUrl+"js/mw_freegift/lib/underscore.js",mw_baseUrl+"js/varien/product.js",mw_baseUrl+"js/varien/configurable.js",mw_baseUrl+"js/mw_freegift/lib/jquery.plugins.min.js",mw_baseUrl+"js/mw_freegift/lib/jquery-migrate-1.2.1.min.js",mw_baseUrl+"js/mw_freegift/lib/backbone-min.js",function(){_.extend(window.FreeGift,Backbone.Events);head.js(mw_baseUrl+"js/mw_freegift/view.js");});
--- data/eretail/test_pages/31dover/pdct_page_test_files/5572.js
+++
@@ -1,1 +0,0 @@
-"undefined"==typeof CE2&&(CE2={}),CE2.uid=135572,CE2.status="no data available";
